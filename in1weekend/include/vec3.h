2734bf6088599332860fec460939e7bab4bbb824
binwatch <h0w1eyu@proton.me>
2022-09-13 08:15:00 +0800

    material abstract class & Lambertian

--- in1weekend/include/vec3.h
+++ in1weekend/include/vec3.h
@@ -53,6 +53,12 @@ class vec3 {
             return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
         }
 
+        bool near_zero() const {
+            // Return true if the vector is close to zero in all dimensions
+            const auto s = 1e-8;
+            return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+        }
+
     public:
         double e[3];
 };
