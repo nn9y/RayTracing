ab49f3842c1b417fb5026dfd6ab0c42bbdcce753
binwatch <h0w1eyu@proton.me>
2022-09-13 08:50:56 +0800

    shiny metal

---
+++ in1weekend/include/metal.h
@@ -0,0 +1,23 @@
+#ifndef METAL_H
+#define METAL_H
+
+#include "material.h"
+#include "hittable.h"
+
+class metal : public material {
+    public:
+        metal(const color& a) : albedo(a) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+    public:
+        color albedo;
+};
+
+#endif
